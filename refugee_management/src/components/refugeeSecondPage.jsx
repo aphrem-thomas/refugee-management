03239648a10f5ce32d761c4dd84533de129933f7
aphrem-thomas <aphremthomas@gmail.com>
2018-06-11 23:23:13 +0530

    added bg image for refugee second page

--- refugee_management/src/components/refugeeSecondPage.jsx
+++ refugee_management/src/components/refugeeSecondPage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-
+import doctor from '../images/doctor1.jpg';
 
 class SecondPage extends React.Component{
     render(){
         return(
             <div className="container cartoonpage h-100">
-                <div className="row d-flex align-items-center h-100">
+                <div className="d-flex justify-content-center mt-3">
+                    <img src={doctor} height="300" width="350"/>
+                </div>
+                <div className="row ">
                     <div className="col-lg-12 col-sm-12 mx-5">
                         <Link to='/refugeesecondpage/refugeesingin' className='p-1'>
                             <button type="button" className="btn btn-primary btn-lg btn-block">Patient</button>
